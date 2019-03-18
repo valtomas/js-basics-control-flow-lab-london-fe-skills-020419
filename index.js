2118c21df0d5c1b0d2415112e865edaf387b00db
Tomas Val <tomas.val@wework.com>
2019-03-18 18:34:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 fuction scuberGreetingForFeet(valueInFeet){
   if (valueInFeet >=400)
-    return "'This one is on me!'"
+    return = "'This one is on me!'"
   
 }
