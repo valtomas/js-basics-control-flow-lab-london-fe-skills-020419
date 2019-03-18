22554cdae2bc86943b1ac5fd3e5d0e843e213ed9
Tomas Val <tomas.val@wework.com>
2019-03-18 18:57:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your code in this file!
+function scuberGreetingForFeet(valueInFeet) {
+  if (valueInFeet <401) {
+    return  "This one is on me!";
+  }
+  
+  else if (valueInFeet >2500){
+    return "No can do."
+  }
+    
+  else if (valueInFeet >2000){
+    return "I will gladly take your thirty bucks."
+  }
+  
+}
+
+function ternaryCheckCity(city){
+  
+  if (city == "NYC") {
+    return "Ok, sounds good."
+  }
+  
+  else {
+    return "No go."
+  }
+  
+}
+
+function switchOnCharmFromTip(tipAmount) {
+  
+ if (tipAmount == "generous") {
+   
+   return "Thank you so much."
+   
+ }
+ else if (tipAmount == "not as generous") {
+   return "Thank you."
+ }
+ else {
+   return "Bye."
+ }
+}
