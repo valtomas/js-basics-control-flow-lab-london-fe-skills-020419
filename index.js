30085fee3d1564a3a503730e2a6a8f6aab684255
Tomas Val <tomas.val@wework.com>
2019-03-18 18:47:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ function scuberGreetingForFeet(valueInFeet) {
     return  "This one is on me!";
   }
   
+  else if (valueInFeet >2500){
+    return "No can do."
+  }
+    
   else if (valueInFeet >2000){
     return "I will gladly take your thirty bucks."
   }
   
-  else if (valueInFeet >2500){
-    return "No can do."
-  }
+   
+
   
 }
